a5668729aefa3081fb036472ac95017a2792ed8e
Levi and Josh <levi+josh@appacdemy.io>
2014-05-28 12:21:03 -0400

    Finish Phase 1, new and edit views

--- app/controllers/cats_controller.rb
+++ app/controllers/cats_controller.rb
@@ -23,11 +23,17 @@ class CatsController < ApplicationController
   end
 
   def edit
-
+    @cat = Cat.find(params[:id])
   end
 
   def update
+    @cat = Cat.find(params[:id])
 
+    if @cat.update(cat_params)
+      redirect_to cat_url(@cat)
+    else
+      render :edit
+    end
   end
 
   def destroy
